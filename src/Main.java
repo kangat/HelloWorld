484768750114372ca733b05b05afc1c6ea2019b9
kangat <1172502199@qq.com>
2017-06-29 22:52:31 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        boolean flag = false;
+        if (!flag){
+            System.out.println("this is a test!");
+        }
+        System.out.println("Hello World!");
+    }
+}
